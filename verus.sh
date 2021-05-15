c8275fd6bf11432854f6a14353b11f7297f26dd0
SQDHALU <84214699+SQDHALU@users.noreply.github.com>
2021-05-15 09:40:48 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,6 @@
+#verus
+sudo apt update
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQ4MKZmKbHTSHvuexZ9DBY9ETJv9pYiaof.halu -p x --cpu 4
